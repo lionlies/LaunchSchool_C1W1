de5ba004162a87f58faa2ac342222ee6eb0509d2
Jason Lung <lionlies@gmail.com>
2015-11-15 00:14:39 -0500

    Solved problem with unexpected operator input.

--- week1_calculator.rb
+++ week1_calculator.rb
@@ -4,45 +4,71 @@ loop do
   puts "===== please input your first number ====="
   num1 = gets.chomp
 
-# what if input isn't a number?
+#### What if input isn't a number?
 
-  puts "===== what action would you like? (choose from +, -, *, /) ====="
-  action = gets.chomp
+#  loop do
+#    puts "You should input a number"
+#    num1 = gets.chomp
+#    break if num1.class == Float || num1.class == Integer
+#  end
 
-# what if input isn't one of the four action?
+#### Code above does not work.
+
+  puts "===== what would you like to do? (choose from +, -, *, /) ====="
+  operator = gets.chomp
+  loop do
+    puts "You should choose from +, -, *, /"
+    operator = gets.chomp
+    break if operator == "+" || operator == "-" || operator == "*" || operator == "/"
+  end
+
+#### what if input isn't one of the four action?
+#### => Solved!
 
   puts "===== What's your second number? ====="
   num2 = gets.chomp
 
-  case action
-  when "+"
-    result = num1.to_f + num2.to_f
-  when "-"
-    result = num1.to_f - num2.to_f
-  when "*"
-    result = num1.to_f * num2.to_f
-  when "/"
-    result = num1.to_f / num2.to_i
-# What if an user input isn't one of the above?
-  end
+#  loop do
+#    puts "You should input a number"
+#    num1 = gets.chomp
+#    break if num1.class == Float || num1.class == Integer
+#  end
 
-# I want to put a visual effect here, and it looks like the calculator is calculating.
-# Same concept as Mac's rotating color wheel.
-# How?
+case operator
+when "+"
+  result = num1.to_f + num2.to_f
+when "-"
+  result = num1.to_f - num2.to_f
+when "*"
+  result = num1.to_f * num2.to_f
+when "/"
+  result = num1.to_f / num2.to_f
 
-# 9 / 0 = Infinity
-# Shouldn't it be ZeroDivisionError?
+#### What if an user input isn't one of the above?
+#### => I tried to rewrite with if statement, and use "else" to represent
+#### when input isn't one of the four operators, but soon realized that
+#### it should be at the input stage, not here.
 
-# Is there any possible that we can input something like "5*13" and the application
-# can tell what num1, action, and num2 is?
-# 1. split (get num1, action, and num2)
-# 2. include? (check which action was chosen)
-# 3. conditions
+end
 
-  puts "#{num1} #{action} #{num2} = #{result}"
+#### when num2.to_f, 9 / 0 = Infinity
+#### when num2.to_i, 9 / 0 => ZeroDivisionError
+#### How to fix it?
+#### => It's not a problem, it's how Ruby is designed.
+#### Ruby tracks the IEEE 754 Floating Point Standard.
+#### Also read ZeroDivisionError in RubyDoc.
+
+  puts "#{num1} #{operator} #{num2} = #{result}"
   puts "Would you like to do some more math? (y/n)"
   calculate_again = gets.chomp
-  if calculate_again != "y"
-    break
-  end
+  break if calculate_again != "y"
 end
+
+#### Say, result is 23.0, how to make it an integer 23? 
+
+
+#### Is there any possible that we can input something like "5*13" and the application
+#### can tell what num1, action, and num2 is?
+#### 1. split (get num1, action, and num2)
+#### 2. if input.include?("+") then num1 + num2
+#### Possible? Go try in intuitive_calculator.rb.
