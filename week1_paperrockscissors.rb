3e0bc20d81b7c432d7358fe3b8feb3633a503524
Jason Lung <lionlies@gmail.com>
2015-11-16 15:01:04 -0500

    Tweaked PRS game

--- week1_paperrockscissors.rb
+++ week1_paperrockscissors.rb
@@ -5,40 +5,41 @@
 # compare
 # ask if playing again
 
+
+CHOICES = {"p" => "Paper", "r"=>"Rock", "s"=>"Scissors"}
 puts "Hi, it's a Tiny Paper, Rock, Scissors game."
 
-loop do
-
-  puts "P for Paper, R for Rock, S for Scissors, please pick one:"
-
-  player_pick = gets.chomp
-  if player_pick.capitalize != "P" && player_pick.capitalize != "R" && player_pick.capitalize != "S"
-    loop do
-      puts "You have to pick among P, R, and S"
-      player_pick = gets.chomp
-      break if player_pick.capitalize == "P" || player_pick.capitalize == "R" || player_pick.capitalize == "S"
-    end
+def display_winning_message(winning_choice)
+  case winning_choice
+  when "r"
+    puts "Rock smashes scissors."
+  when "s"
+    puts "Scissors cuts Paper."
+  when "p"
+    puts "Paper wraps rock."
   end
+end
 
-  computer_pick = ["P", "R", "S"].sample
+loop do
+  begin
+    puts "P for Paper, R for Rock, S for Scissors, please pick one:"
+    player_pick = gets.chomp.downcase
+  end until CHOICES.keys.include?(player_pick)
 
-  if player_pick.capitalize == "R" && computer_pick == "S"
-    puts "You pick Rock and computer picks Scissocrs, you win."
-  elsif player_pick.capitalize == "P" && computer_pick == "R"
-    puts "You pick Paper and computer picks Rock, you win."
-  elsif player_pick.capitalize == "S" && computer_pick == "P"
-    puts "You pick Scissors and computer picks Paper, you win."
-  elsif player_pick.capitalize == "P" && computer_pick == "S"
-    puts "You pick Paper and computer picks Scissocrs, computer wins."
-  elsif player_pick.capitalize == "R" && computer_pick == "P"
-    puts "You pick Rock and computer picks Paper, computer wins."
-  elsif player_pick.capitalize == "S" && computer_pick == "R"
-    puts "You pick Scissors and computer picks Rock, computer wins."
-  else
+  computer_pick = CHOICES.keys.sample
+
+  if player_pick == computer_pick
     puts "It's a tie."
+  elsif (player_pick == "r" && computer_pick == "s") || (player_pick == "p" && computer_pick == "r") || (player_pick == "s" && computer_pick == "p")
+    display_winning_message(player_pick)
+    puts "You won."
+  else
+    display_winning_message(computer_pick)
+    puts "Computer wins."
   end
 
   puts "Would you like to play again?(y/n)"
-  play_again = gets.chomp
-  break if play_again.capitalize == "N"
+  break if gets.chomp.downcase != "y"
 end
+
+puts "See you soon~"
