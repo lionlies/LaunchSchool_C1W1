57e5b012bd8c09d8ab15415dd81ba260030f9d86
Jason Lung <lionlies@gmail.com>
2015-11-16 13:06:07 -0500

    Fixed calculator bug, finished PRS game.

--- week1_calculator.rb
+++ week1_calculator.rb
@@ -1,62 +1,47 @@
-puts "It's a tiny calulator, what would you like to do?"
+puts "It's a tiny calulator."
 
 loop do
+
   puts "===== please input your first number ====="
   num1 = gets.chomp
-
-#### What if input isn't a number?
-
-#  loop do
-#    puts "You should input a number"
-#    num1 = gets.chomp
-#    break if num1.class == Float || num1.class == Integer
-#  end
-
-#### Code above does not work.
+  if num1.to_i.to_s != num1 && num1.to_f.to_s != num1
+    loop do
+      puts "You should input a number"
+      num1 = gets.chomp
+      break if num1.to_i.to_s == num1 || num1.to_f.to_s == num1
+    end
+  end
 
   puts "===== what would you like to do? (choose from +, -, *, /) ====="
   operator = gets.chomp
-  loop do
-    puts "You should choose from +, -, *, /"
-    operator = gets.chomp
-    break if operator == "+" || operator == "-" || operator == "*" || operator == "/"
+  if operator != "+" && operator != "-" && operator != "*" && operator != "/"
+    loop do
+      puts "You should choose from +, -, *, /"
+      operator = gets.chomp
+      break if operator == "+" || operator == "-" || operator == "*" || operator == "/"
+    end
   end
-
-#### what if input isn't one of the four action?
-#### => Solved!
 
   puts "===== What's your second number? ====="
   num2 = gets.chomp
+  if num2.to_i.to_s != num2 && num2.to_f.to_s != num2
+    loop do
+      puts "You should input a number"
+      num2 = gets.chomp
+      break if num2.to_i.to_s == num2 || num2.to_f.to_s == num2
+    end
+  end
 
-#  loop do
-#    puts "You should input a number"
-#    num1 = gets.chomp
-#    break if num1.class == Float || num1.class == Integer
-#  end
-
-case operator
-when "+"
-  result = num1.to_f + num2.to_f
-when "-"
-  result = num1.to_f - num2.to_f
-when "*"
-  result = num1.to_f * num2.to_f
-when "/"
-  result = num1.to_f / num2.to_f
-
-#### What if an user input isn't one of the above?
-#### => I tried to rewrite with if statement, and use "else" to represent
-#### when input isn't one of the four operators, but soon realized that
-#### it should be at the input stage, not here.
-
-end
-
-#### when num2.to_f, 9 / 0 = Infinity
-#### when num2.to_i, 9 / 0 => ZeroDivisionError
-#### How to fix it?
-#### => It's not a problem, it's how Ruby is designed.
-#### Ruby tracks the IEEE 754 Floating Point Standard.
-#### Also read ZeroDivisionError in RubyDoc.
+  case operator
+  when "+"
+    result = num1.to_f + num2.to_f
+  when "-"
+    result = num1.to_f - num2.to_f
+  when "*"
+    result = num1.to_f * num2.to_f
+  when "/"
+    result = num1.to_f / num2.to_f
+  end
 
   puts "#{num1} #{operator} #{num2} = #{result}"
   puts "Would you like to do some more math? (y/n)"
@@ -64,11 +49,23 @@ end
   break if calculate_again != "y"
 end
 
-#### Say, result is 23.0, how to make it an integer 23? 
+# What if an user input isn't one of the four operators?
+# => I tried to rewrite with if statement, and use "else" to represent
+# when input isn't one of the four operators, but soon realized that
+# it should be at the input stage, not here.
+# By the way, "else" can be used with "when" in case statement.
 
+# when num2.to_f, 9 / 0 = Infinity
+# when num2.to_i, 9 / 0 => ZeroDivisionError
+# How to fix it?
+# => It's not a problem, it's how Ruby is designed.
+# Ruby tracks the IEEE 754 Floating Point Standard.
+# Also read ZeroDivisionError in RubyDoc.
 
-#### Is there any possible that we can input something like "5*13" and the application
-#### can tell what num1, action, and num2 is?
-#### 1. split (get num1, action, and num2)
-#### 2. if input.include?("+") then num1 + num2
-#### Possible? Go try in intuitive_calculator.rb.
+# Say, result is 23.0, how to make it show as an integer 23?
+
+# Is there any possible that we can input something like "5*13" and the application
+# can tell what num1, action, and num2 is?
+# 1. split (get num1, action, and num2)
+# 2. if input.include?("+") then num1 + num2
+# Possible? Go try in intuitive_calculator.rb
--- week1_paperrockscissors.rb
+++ week1_paperrockscissors.rb
@@ -1,6 +1,44 @@
 # sudo code:
 # conditions: rock > scissors, paper > rock, scissors > paper, and tie.
 # player one choose an action (r/p/s)
-# plaer two choose an action (r/p/s)
+# computer choose an action randomly (r/p/s)
 # compare
 # ask if playing again
+
+puts "Hi, it's a Tiny Paper, Rock, Scissors game."
+
+loop do
+
+  puts "P for Paper, R for Rock, S for Scissors, please pick one:"
+
+  player_pick = gets.chomp
+  if player_pick.capitalize != "P" && player_pick.capitalize != "R" && player_pick.capitalize != "S"
+    loop do
+      puts "You have to pick among P, R, and S"
+      player_pick = gets.chomp
+      break if player_pick.capitalize == "P" || player_pick.capitalize == "R" || player_pick.capitalize == "S"
+    end
+  end
+
+  computer_pick = ["P", "R", "S"].sample
+
+  if player_pick.capitalize == "R" && computer_pick == "S"
+    puts "You pick Rock and computer picks Scissocrs, you win."
+  elsif player_pick.capitalize == "P" && computer_pick == "R"
+    puts "You pick Paper and computer picks Rock, you win."
+  elsif player_pick.capitalize == "S" && computer_pick == "P"
+    puts "You pick Scissors and computer picks Paper, you win."
+  elsif player_pick.capitalize == "P" && computer_pick == "S"
+    puts "You pick Paper and computer picks Scissocrs, computer wins."
+  elsif player_pick.capitalize == "R" && computer_pick == "P"
+    puts "You pick Rock and computer picks Paper, computer wins."
+  elsif player_pick.capitalize == "S" && computer_pick == "R"
+    puts "You pick Scissors and computer picks Rock, computer wins."
+  else
+    puts "It's a tie."
+  end
+
+  puts "Would you like to play again?(y/n)"
+  play_again = gets.chomp
+  break if play_again.capitalize == "N"
+end
