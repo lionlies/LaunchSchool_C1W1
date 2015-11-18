ef2cbefefdfe73060c1174d00688f0a69a74383c
Jason Lung <lionlies@gmail.com>
2015-11-18 15:02:53 -0500

    Refactoring tictactoe game.

--- week1_tictactoe.rb
+++ week1_tictactoe.rb
@@ -2,7 +2,6 @@
 sudo code:
 
 draw a board
-
 begin
   player picks an empty square using marker "X"
   check winner
@@ -10,12 +9,19 @@ begin
   check winner
 end until there's a winner or all squares are taken
 
-What needs to be tweaked:
-1. All winning conditions
-2. restrict from choosing occupied squares.
-3. Rondomly choose player or computer to be the first player.
-4. A.I.
+More:
+[v] Add all winning conditions
+[v] Restrict from choosing occupied squares.
+[v] Play again?
+[v] First player has advantage, rondomly choose the first player.
+[v] Fix the logical problem when checking winner.
+
+- two_in_a_row => How to use this method?
 =end
+
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+                 [1, 4, 7], [2, 5, 8], [3, 6, 9],
+                 [1, 5, 9], [3, 5, 7]]
 
 def draw_board(b)
   system "clear"
@@ -37,8 +43,10 @@ def empty_positions(b)
 end
 
 def player_picks_square(b)
-  puts "Pick a square: (1 - 9)"
-  position = gets.chomp.to_i
+  begin
+    puts "Pick a square: (1 - 9)"
+    position = gets.chomp.to_i
+  end until empty_positions(b).include?(position)
   b[position] = "X"
 end
 
@@ -48,32 +56,68 @@ def computer_picks_square(b)
 end
 
 def check_winner(b)
-  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-  winning_lines.each do |line|
-    if b[line[0]] == "X" && b[line[1]] == "X" && b[line[2]] == "X"
-      return "Player"
-    elsif b[line[0]] == "O" && b[line[1]] == "O" && b[line[2]] == "O"
-      return "Computer"
-    else
-      return nil
-    end
+  WINNING_LINES.each do |line|
+    return "Player" if b.values_at(*line).count("X") == 3
+    return "Computer" if b.values_at(*line).count("O") == 3
+# return "Player" if b.values_at(*line) == ["X", "X", "X"]
+# return "Computer" if b.values_at(*line) == ["O", "O", "O"]
+  end
+  nil
+end
+
+def two_in_a_row(hsh, mrkr)
+  if hsh.values.count(mrkr) == 2  # If coding like line 62 & 63, it'll be more complicated.
+    hsh.select{|k,v| v == ' '}.keys.first
+  else
+    false
   end
 end
 
-board = initialize_board
-draw_board(board)
+
+def game_engine
+  board = initialize_board
+  draw_board(board)
+  first_player = ["Player", "Computer"].sample
+
+  if first_player == "Player"
+    begin
+      player_picks_square(board)
+      draw_board(board)
+      winner = check_winner(board)
+      if winner || empty_positions(board).empty?
+        break
+      end
+      computer_picks_square(board)
+      draw_board(board)
+      winner = check_winner(board)
+    end until winner || empty_positions(board).empty?
+  # Even if I win, sometimes program still shows "Computer win."
+
+  elsif first_player == "Computer"
+    begin
+      computer_picks_square(board)
+      draw_board(board)
+      winner = check_winner(board)
+      if winner || empty_positions(board).empty?
+        break
+      end
+      player_picks_square(board)
+      draw_board(board)
+      winner = check_winner(board)
+    end until winner || empty_positions(board).empty?
+  end
+
+  if winner
+    puts "#{winner} won!"
+  else
+    puts "It's a tie."
+  end
+end
+
 
 begin
-  player_picks_square(board)
-  draw_board(board)
-  winner = check_winner(board)
-  computer_picks_square(board)
-  draw_board(board)
-  winner = check_winner(board)
-end until winner || empty_positions(board).empty?
+  game_engine
+  puts "Play again? (y/n)"
+end until gets.chomp.downcase != "y"
 
-if winner
-  puts "#{winner} won!"
-else
-  puts "It's a tie."
-end
+puts "See you soon!"
